59883de8515faff658f0a9c421ae25dd675f10f5
Boris heifits <hef1995@gmail.com>
2023-11-24 19:41:40 +0200

    new project

--- new_library.py
+++ new_library.py
@@ -1,3 +1,4 @@
+from datetime import timedelta, datetime
 import json
 
 class Book:
@@ -11,6 +12,9 @@ class Book:
             self.max_loan_days = max_loan_days
             self.available = True
             self.loan_history = []
+
+        def __str__(self):
+            return f"Book: {self.title} by {self.author} ({self.year})"
 
         def take(self):
             if self.available:
@@ -26,6 +30,18 @@ class Book:
                 return True
             else:
                 print(f"The book '{self.title}' is not checked out.")
+                return False
+        
+        def take_book(self, borrower_name):
+            if self.available:
+                self.available = False
+                loan_date = datetime.now()
+                due_date = loan_date + timedelta(days=self.max_loan_days)
+                self.loan_history.append({'borrower': borrower_name, 'loan_date': loan_date, 'due_date': due_date})
+                print(f"Book '{self.title}' taken by {borrower_name}. Due date: {due_date.strftime('%Y-%m-%d')}")
+                return True
+            else:
+                print(f"The book '{self.title}' is not available for loan.")
                 return False
 
 class Customers:
@@ -62,25 +78,104 @@ class Library:
         with open('customers.json', 'w') as customers:
             json.dump(self.customers, customers, indent=2)
 
-    def add_book(self, serie, title, author, year):
-        self.books[serie] = {'title': title, 'author': author, 'year': year}
-        print(f"Book with from the {serie} added successfully!")
+    def add_book(self, series, title, author, year):
+        self.books[series] = {'title': title, 'author': author, 'year': year, 'available': True}
+        print(f"Book with from the {series} added successfully!")
 
     def add_customer(self, customer_id, name, age, adress, email):
         self.customers[customer_id] = {'Name': name, 'Age': age, 'Adress': adress, "Email": email}
         print(f"Customer with ID {customer_id} added successfully!")
 
+    def check_if_avilable(self, series):
+        if series in self.books:
+            return self.books[series]['available']
+        else:
+            print(f'the book from series {series} was not found')
+
+    def show_all_books(self):
+        for series, book_info in self.books.items():
+            print(f"Series: {series}, Title: {book_info['title']}, Author: {book_info['author']}, Year: {book_info['year']}, Available: {book_info['available']}")
+
+
+
+
+
+
+
+
+
+
+
+class libraryUI:
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 my_library = Library('my library', 'peer 78,haifa')
-
 my_library.add_book("Harry potter", "The chamber", 'J.k rolling', 2001)
-my_library.save_book()
-my_library.load_books()
+my_library.add_book(series='ABC123', title='Python Programming', author='John Doe', year=2022)
 
-my_library.add_customer(1, 'boris', 28, 'peer 78, haifa', 'bbb@com')
-my_library.save_customer()
-my_library.load_customers()
+
+# book_series_to_check = 'boris'
+
+# if my_library.check_if_avilable(book_series_to_check):
+#     print('available')
+# else:
+#     print('not')
+
+# my_library.save_book()
+
+# my_library.show_all_books()
+
+
+
+
+
+# my_library.save_book()
+# my_library.load_books()
+
+# my_library.add_customer(1, 'boris', 28, 'peer 78, haifa', 'bbb@com')
+# my_library.save_customer()
+# my_library.load_customers()
 
 
 
